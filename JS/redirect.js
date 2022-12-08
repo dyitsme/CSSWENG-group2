f00e4b5b297572a89b51cf5168ac3fae915bc2a1
Chunkiee05 <juan_azarcon@dlsu.edu.ph>
2022-12-08 13:11:24 +0800

    block special char in renaming and create folder

--- JS/redirect.js
+++ JS/redirect.js
@@ -384,3 +384,11 @@ function formatSize(bytes, decimals = 2) {
 
     return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
 }
+
+function disableSpecialChar(e) {
+      if(!((e.keyCode >= 65) && (e.keyCode <= 90) || (e.keyCode >= 97) && (e.keyCode <= 122) || (e.keyCode >= 48) && (e.keyCode <= 57))){
+         e.returnValue = false;
+         return;
+      }
+      e.returnValue = true;
+}
